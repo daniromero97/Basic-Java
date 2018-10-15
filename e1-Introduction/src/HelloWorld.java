17005931b5d08a575b764d5729daf084e2f6ad7a
Daniel <danielromeroalaman@gmail.com>
2018-10-15 16:17:05 +0200

    e1-Introduction

---
+++ e1-Introduction/src/HelloWorld.java
@@ -0,0 +1,21 @@
+/*
+    Author: Daniel Romero
+    Exercise: show by console: "Hello world!!"
+*/
+
+// Class HelloWorld
+public class HelloWorld
+{
+    // Methods
+    void init()
+    {
+        // It shows “Hello world!!”
+        System.out.println("Hello world!!");
+    }
+
+    // Main method
+    public static void main (String args[])
+    {
+        new HelloWorld().init();
+    }
+}
