00651a390c0b7368d64403ee963658f30624fc79
Daniel <danielromeroalaman@gmail.com>
2018-10-20 17:41:01 +0200

    e52-NestedLoopExercise (drawing of figures)

---
+++ e52-NestedLoopExercise/src/NestedLoopExercise.java
@@ -0,0 +1,34 @@
+/*
+    Author: Drowsiel Romero
+    Exercise: drawing of figures
+*/
+
+
+// Class NestedLoopExercise
+public class NestedLoopExercise
+{
+    void init()
+    {
+        String s="";
+        int rows = 11, columns = 11;
+
+        for(int i=0;i<rows;i++)
+        {
+            for(int j=0;j<columns;j++)
+            {
+                if ((i+j==rows/2) || (j-i==rows/2) || (i-j==rows/2) || (i+j==3*(rows/2))) s+="* ";
+                else if ((i+j<rows/2) || (j-i>rows/2) || (i-j>rows/2) || (i+j>3*(rows/2))) s+="  ";
+                else s=s+ "+ ";
+            }
+            s+="\n";
+        }
+
+        System.out.println(s);
+    }
+
+    // Main method
+    public static void main (String args[])
+    {
+        new NestedLoopExercise().init();
+    }
+}
